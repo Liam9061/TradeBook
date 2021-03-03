321076f474709ff4271e03a89fe1a13317c5d64e
liam9061 <60947617+Liam9061@users.noreply.github.com>
2021-03-03 16:02:23 +0000

    Added Database

--- TradeBook.DataAccess/Data/ApplicationDbContext.cs
+++ TradeBook.DataAccess/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TradeBook.Models;
 
 namespace TradeBook.DataAccess.Data
 {
@@ -12,5 +13,6 @@ namespace TradeBook.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ TradebookDB/Category.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Categories]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL
+)
